b7e171e00c22e8bb59a3259d4e15c3c72c276bb2
Bhargavi Deotare <90917776+BhargaviDeotare@users.noreply.github.com>
2023-01-30 09:44:38 +0530

    Update keys.js

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoUrl:
+    "mongodb://Bhargavi1234:Bhargavi1234@ac-1tymudn-shard-00-00.qfopky9.mongodb.net:27017,ac-1tymudn-shard-00-01.qfopky9.mongodb.net:27017,ac-1tymudn-shard-00-02.qfopky9.mongodb.net:27017/?ssl=true&replicaSet=atlas-zukf2k-shard-0&authSource=admin&retryWrites=true&w=majority",
+
+  Jwt_secret: "faslkfocvneofu",
+};
